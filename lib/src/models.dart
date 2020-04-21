35f0c0f0022d39d867ae72a73e47c9603e2fbb46
Robert3141 <robert.aries1@gmail.com>
2020-04-21 18:23:38 +0100

    Format code in models.dart

--- lib/src/models.dart
+++ lib/src/models.dart
@@ -21,29 +21,38 @@ class ConnectedClientNode {
 
 /// The type of data that is sent and received. It includes all the neccessary information
 class DataPacket {
-
-  DataPacket({@required this.name, @required this.host, @required this.port, @required this.title, this.payload});
+  DataPacket(
+      {@required this.name,
+      @required this.host,
+      @required this.port,
+      @required this.title,
+      this.payload});
 
   DataPacket.fromJson(Map<String, dynamic> json)
-    : this.host = json["host"],
-      this.port = int.parse(json["port"]),
-      this.name = json["name"],
-      this.title = json["title"],
-      this.payload = json["payload"];
+      : this.host = json["host"],
+        this.port = int.parse(json["port"]),
+        this.name = json["name"],
+        this.title = json["title"],
+        this.payload = json["payload"];
 
   /// The IP adress of the sender
   final String host;
+
   /// The Port being used by the sender (and reciever)
   final int port;
+
   /// The name of the sender
   final String name;
+
   /// The title of the packet
   final String title;
+
   /// The actual data being ditributed
   final dynamic payload;
 
   /// Encodes the packet data into a json ready for transmitting
-  String encodeToString() =>'{"host":"$host", "port": "$port", "name": "$name", "title": "$title", "payload": "$payload"}';
+  String encodeToString() =>
+      '{"host":"$host", "port": "$port", "name": "$name", "title": "$title", "payload": "$payload"}';
 }
 
 Future<HttpResponse> _sendHandler(HttpRequest request, IsoLogger log) async {
@@ -54,7 +63,8 @@ Future<HttpResponse> _sendHandler(HttpRequest request, IsoLogger log) async {
   return request.response;
 }
 
-Future<HttpResponse> _responseHandler(HttpRequest request, IsoLogger log) async {
+Future<HttpResponse> _responseHandler(
+    HttpRequest request, IsoLogger log) async {
   final content = await utf8.decoder.bind(request).join();
   dynamic c = content;
   try {
